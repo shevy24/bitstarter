0d2c25feca634aae3f7b794016974d0f3a9a03c0
sherif <shevy24@gmail.com>
2013-09-11 12:18:50 +0000

    Added new web.js

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var fs = require ('fs');
 var express = require('express');
 var app = express();
+var htmlfile = "index.html";
 
 app.use(express.static(__dirname + '/assets'));
 
@@ -11,7 +12,10 @@ app.get('/', function(request, response) {
 });
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
